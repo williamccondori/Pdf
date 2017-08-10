1a4b6ba9a329f144ffc1264e990ebfa92b1bb27b
William Condori Quispe <williamccondori@outlook.com>
2017-08-10 15:34:28 -0500

    Ejemplo

---
+++ Pdf/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pdf.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase archivo)
+        {
+
+            BinaryReader theReader = new BinaryReader(archivo.InputStream);
+            byte[] thePictureAsBytes = theReader.ReadBytes(archivo.ContentLength);
+
+            string base64 = Convert.ToBase64String(thePictureAsBytes);
+
+            TempData["Archivo"] = base64;
+
+            return RedirectToAction("About");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Pdf/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pdf.MvcApplication" Language="C#" %>
